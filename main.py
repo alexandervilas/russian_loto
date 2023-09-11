db17c4976036a288dc9fbbe3f5340616c6c5b4d5
Human72 <ksokolov436@gmail.com>
2023-09-11 19:45:46 +0300

    generate_card

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-#Это будет простая игра в русское лото
+# import random
 
+# def create_loto_card():
+#     card = []
+
+#     for i in range(3):
+#         row = random.sample(range(1, 91), 5)
+#         card.append(row)
+
+# или
 
 import random
 
 def generate_card():
 card = random.sample(range(1, 91), 9)
 return card
+
