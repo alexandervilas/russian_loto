2d29a7f6669bc098a78f836a8fe151a13d4f8e68
Igor <jamesjohna07@gmail.com>
2023-09-11 20:09:56 +0300

    123

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ def create_dict_with_lists(num_lists):
 my_dict = create_dict_with_lists(3)
 print(my_dict)
 
+import random
+
+number = [i for i in range(1, 91)]
+card = sorted(random.sample(number, 15))
+def winner_found():
+    if len(card) == 0:
+        return True
+    return False
+
+def main():
+    while (not winner_found()):
+        print("\n### NEW GAME ###")
+        random_choice = number.pop()
+        print("The random lotto is: {} ".format(random_choice))
+        if random_choice in card:
+            card.remove(random_choice)
