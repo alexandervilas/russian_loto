049b479a45a0bc44a2efbf8e3f0f70cf140a8a53
Igor <jamesjohna07@gmail.com>
2023-09-11 20:25:00 +0300

    Добавление комментариев

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ print(my_dict)
 
 import random
 
-number = [i for i in range(1, 91)]
-card = sorted(random.sample(number, 15))
-def winner_found():
+number = [i for i in range(1, 91)]  #Здесь я добавил диапазон возможных чисел
+card = sorted(random.sample(number, 15)) #здесь я указываю, что в карточке будут рандомные числа в нашем диапазоне
+def winner_found(): #создаю функцию по поиску победителя, победитель находится тогда, когда длина карточки начинает равнятся нулю
     if len(card) == 0:
         return True
-    return False
+    return False #пока длина карточки не равняется нулю, мы продолжаем играть 
 
-def main():
-    while (not winner_found()):
+def main(): #создаем функцию самой игры, уже с самой игрой
+    while (not winner_found()): #пока победитель не найден 
         print("\n### NEW GAME ###")
-        random_choice = number.pop()
+        random_choice = number.pop() #выпадает случайное число и если оно есть, то его удаляем по индексу с длины карточки 
         print("The random lotto is: {} ".format(random_choice))
         if random_choice in card:
-            card.remove(random_choice)
+            card.remove(random_choice) #удаляет первый элемент последовательности по назначению
