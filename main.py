1afc9fd8655ef4404ef68e56274d49918e9632c6
Flex333444 <144368931+Flex333444@users.noreply.github.com>
2023-09-11 19:37:25 +0300

    Словарь списков

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #Это будет простая игра в русское лото
+def create_dict_with_lists(num_lists):
+    """
+   
+    """
+    result_dict = {}
+    for i in range(num_lists):
+        result_dict[f'list_{i+1}'] = []
+    return result_dict
 
+
+my_dict = create_dict_with_lists(3)
+print(my_dict)
 
